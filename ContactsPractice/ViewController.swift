e84000a4beaa97c7ef244f9740b308d8b8d9ba0c
tardes <jmo.gonzalez@gmail.com>
2026-01-29 16:43:56 +0100

    Initial Commit

---
+++ ContactsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsPractice
+//
+//  Created by Tardes on 29/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
